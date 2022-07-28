d77b518e61bd1cc5007969e4a31526cb1b27e1b2
2483428575 <2483428575@qq.com>
2022-07-28 16:16:59 +0800

    init

---
+++ src/test/java/com/jiawa/wiki/WiKiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiKiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
