db251262061b49e79b288203f286b77277a497f2
2483428575 <2483428575@qq.com>
2022-07-29 01:17:16 +0800

    2-7 增加hello测试接口test，http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,9 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post, " + name;
     }
 }
