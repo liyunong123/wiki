54018fcaf557f437176b5dc3837ca65a34039f4d
2483428575 <2483428575@qq.com>
2022-07-30 11:36:02 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
-import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.req.EbookResp;
 import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
