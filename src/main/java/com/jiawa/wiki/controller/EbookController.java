05299814fac6d2feb93c453e9bf4e2fce4992850
2483428575 <2483428575@qq.com>
2022-07-30 10:46:37 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,20 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class EbookController {
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/ebook/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
