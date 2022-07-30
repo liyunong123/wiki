9176e75b37922a9d43b0ed6e547e1f0ae78907af
2483428575 <2483428575@qq.com>
2022-07-31 00:31:02 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Spring Boot + Vue3电子书系统
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
