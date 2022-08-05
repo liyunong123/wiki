3393522a1fd887ad387bb9340550a91a44ab507c
2483428575 <2483428575@qq.com>
2022-08-06 01:39:03 +0800

    6-11 为保存接口增加参数校验

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -25,7 +25,7 @@ public class EbookController {
         return resp;
     }
     @GetMapping("/save")
-    public CommonResp save(@RequestBody EbookSaveReq req){
+    public CommonResp save(@Valid @RequestBody EbookSaveReq req){
         CommonResp resp = new CommonResp<>();
          ebookService.save(req);
         return resp;
